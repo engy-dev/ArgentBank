9805302315e152640e4c02e322460f788f72f9de
Engy Essam <153827798+engy-dev@users.noreply.github.com>
2024-07-20 17:36:54 +0200

    solving affichage firstName + mise en place firstname lastname header + user

--- projet-argentbank/src/components/usercomponents/user.js
+++ projet-argentbank/src/components/usercomponents/user.js
@@ -1,67 +1,74 @@
-import { connect, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import { fetchedUser } from "../../services/redux/fetch/fetcheduser";
-import EditUserName from "./edituser";
-import Changenamestyle from "../../styles/userstyles/changenamestyle";
-import Index from "../../pages/indexpage";
+import { connect, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { fetchedUser } from '../../services/redux/fetch/fetcheduser';
+import EditUserName from './edituser';
+import Changenamestyle from '../../styles/userstyles/changenamestyle';
+import Index from '../../pages/indexpage';
 
-const User = ({token, fetchedUser }) => {
-  const [isToggleBtn, setIsToggleBtn] = useState(true);
-  const [isToggle, setIsToggle] = useState(false);
-  const firstName = useSelector((state)=>state.firstName);
+const User = ({ token, fetchedUser }) => {
+    const [isToggleBtn, setIsToggleBtn] = useState(true);
+    const [isToggle, setIsToggle] = useState(false);
+    const user = useSelector((state) => state.user)
 
-  
+    console.log(user)
 
-  useEffect(() => {
-    if (token) {
-      const request = {
-        method: "POST",
-        endPoints: "profile",
-        token: token,
-        fistName: firstName
-      };
+    useEffect(() => {
+        if (token) {
+            const request = {
+                method: 'POST',
+                endPoints: 'profile',
+                token: token,
+            };
 
-      fetchedUser(request);
-    }
-  }, [token, fetchedUser, firstName]);
-
-  
-      const change = () => {
+            fetchedUser(request);
+        }
+    }, [token, fetchedUser]);
+    const change = () => {
         setIsToggleBtn(!isToggleBtn);
         setIsToggle(!isToggle);
-      };
+    };
 
-       if (!token) {
-        return <Index/>;
-      }
+    if (!token) {
+        return <Index />;
+    }
 
-      return (
+    return (
         <>
-        <div className="header">
-          <h1>Welcome back</h1>
-          <div style={{display: isToggleBtn ? "flex": "none"}}>
-            <h2>{firstName}</h2>
-          </div>
-         <button onClick={()=> change()}  className="edit-button" style={{display: isToggleBtn ? "flex": "none"}}>Edit Name </button>
-        </div>
+            <div className="header">
+                <h1>Welcome back</h1>
+                <div style={{ display: isToggleBtn ? 'flex' : 'none' }}>
+                    <h2>
+                        {user.firstName}&nbsp;
+                        {user.lastName}
+                    </h2>
+                </div>
+                <button
+                    onClick={() => change()}
+                    className="edit-button"
+                    style={{ display: isToggleBtn ? 'flex' : 'none' }}
+                >
+                    Edit Name{' '}
+                </button>
+            </div>
 
-<EditUserName token={token} change={change} isToggle={isToggle}/><Changenamestyle/>
-
+            <EditUserName token={token} change={change} isToggle={isToggle} />
+            <Changenamestyle />
         </>
-      );
+    );
 };
 
-const mapStateToProps = ({token, user}) => {
-  return{
-    token,
-    user
-  };
+const mapStateToProps = ({ token, firstName, lastName }) => {
+    return {
+        token,
+        firstName,
+        lastName,
+    };
 };
 
-const mapDispatchToProps = (dispatch)=> {
-  return {
-    fetchedUser: (...args) => dispatch(fetchedUser(...args))
-  };
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchedUser: (...args) => dispatch(fetchedUser(...args)),
+    };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(User)
+export default connect(mapStateToProps, mapDispatchToProps)(User);
--- projet-argentbank/src/services/redux/fetch/fetcheduser.js
+++ projet-argentbank/src/services/redux/fetch/fetcheduser.js
@@ -1,31 +1,33 @@
-import { login } from "../actions";
+import { login } from '../actions';
 
 export const fetchedUser = (request) => {
-  return async (dispatch) => {
-    return fetch(`http://localhost:3001/api/v1/user/${request.endPoints}`, {
-      method: request.method,
-      headers: {
-        Authorization: `Bearer ${request.token.body.token}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error fetch user !");
-        }
+    return async (dispatch) => {
+        return fetch(`http://localhost:3001/api/v1/user/${request.endPoints}`, {
+            method: request.method,
+            headers: {
+                Authorization: `Bearer ${request.token.body.token}`,
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Error fetch user !');
+                }
 
-        return response.json();
-      })
-      .then((user) => {
-        if (request.token) {
-          user.token = request.token;
-          user.firstName = request.firstName;
-          user.lastName = request.lastName;
-        }
+                return response.json();
+            })
+            .then((user) => {
+                console.log(user.body.firstName);
+                if (request.token) {
+                    user.token = request.token;
+                    request.body.firstName = user.body.firstName;
+                    request.body.lastName = user.body.lastName;
+                    dispatch(login(user));
+                    console.log(user)
+                }
 
-        dispatch(login(user));
-      })
-      .catch((userError) => {
-        console.log(userError);
-      });
-  };
+            })
+            .catch((userError) => {
+                console.log(userError);
+            });
+    };
 };
